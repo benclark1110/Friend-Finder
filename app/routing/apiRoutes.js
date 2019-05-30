326a36e95c6b987c383741e7e05a53e1f7a54c46
benclark1110 <benclark1110@gmail.com>
2019-05-30 19:34:52 -0400

    setting up html and api routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,29 @@
-app.get("/api/reservations", function(req, res) {
-    return res.json(currentReservations);
-});
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
 
-app.get("/api/waitlist", function(req, res) {
-    return res.json(waitList);
-});
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    res.json(waitListData);
+  });
+};
